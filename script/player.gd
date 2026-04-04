4fd616a990700c19c3b04a72ed4901ce9575121b
Honeylol <animatickid@gmail.com>
2026-04-04 13:00:07 -0400

    made simple player with no sprites
    the player has a script and a colision block, allowing the player to look against the wall!

---
+++ script/player.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+#speed
+const SPEED = 300.0
+
+#movement
+func movement():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * SPEED
+	
+#calling functions!
+func _physics_process(_delta: float) -> void:
+	movement()
+	move_and_slide()
+	
+#animation for later
