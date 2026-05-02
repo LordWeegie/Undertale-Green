4aa9042b9bf519e375f2239f38fbdae27890deb3
Mr Coconut <bradley@sellers-blais.com>
2026-05-02 12:10:03 -0700

    Text doesnt cover character

--- scripts/text_background.gd
+++ scripts/text_background.gd
@@ -12,3 +12,14 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if camera:
 		$TextBackground.position = Vector2(get_viewport().get_visible_rect().size.x / 2, get_viewport().get_visible_rect().size.y - 50)
+
+
+func _on_check_player_bottom_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		print("text 2")
+		offset = Vector2(39.48, -450.5)
+
+func _on_check_player_top_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		print("text")
+		offset = Vector2(39.48, -88.5)
