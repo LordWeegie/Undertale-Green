620991f26af563d4e0b7df996d1fa2d2cf674e94
Mr Coconut <bradley@sellers-blais.com>
2026-05-03 09:10:11 -0700

    Added simple system to track soul health!

--- scripts/player_soul/player.gd
+++ scripts/player_soul/player.gd
@@ -2,9 +2,14 @@ extends CharacterBody2D
 
 #speed
 const SPEED = 150.0
-
+@export var active_health : int = 20
+@export var health_max : int = 20
+@export var health_label : Label
 #movement
 func movement():
+	if Input.is_action_just_pressed("select"):
+		active_health -= 3
+		print(active_health)
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * SPEED
 	
@@ -12,5 +17,10 @@ func movement():
 func _physics_process(_delta: float) -> void:
 	movement()
 	move_and_slide()
-	
+	check_health()
+
+func check_health():
+	if active_health <= 0:
+		get_tree().reload_current_scene()
+	health_label.text = "Health: " + str(active_health) + "/" + str(health_max)
 #animation for later
