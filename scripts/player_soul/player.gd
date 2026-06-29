99fd44b1ea6632cdb46229a9927f52bdbf39e663
Mr Coconut <bradley@sellers-blais.com>
2026-06-29 08:44:59 -0700

    Updated to godot 4.7

--- scripts/player_soul/player.gd
+++ scripts/player_soul/player.gd
@@ -20,28 +20,28 @@ func movement():
 		active_health -= 3
 		print(active_health)
 	if Input.is_action_just_pressed("left"):
-		facing_down = true
-		facing_left = false
-		facing_right = false
-		facing_up = false
-
-	if Input.is_action_just_pressed("right"):
-		facing_up = true
 		facing_down = false
-		facing_left = false
-		facing_right = false
-
-	if Input.is_action_just_pressed("up"):
-		facing_up = false
-		facing_down = false
-		facing_right = false
 		facing_left = true
-
-	if Input.is_action_just_pressed("down"):
-		facing_down = false
+		facing_right = false
 		facing_up = false
+
+	elif Input.is_action_just_pressed("right"):
+		facing_up = false
+		facing_down = false
 		facing_left = false
 		facing_right = true
+
+	elif Input.is_action_just_pressed("up"):
+		facing_up = true
+		facing_down = false
+		facing_right = false
+		facing_left = false
+
+	elif Input.is_action_just_pressed("down"):
+		facing_down = true
+		facing_up = false
+		facing_left = false
+		facing_right = false
 		
 	#var input_direction = Input.get_vector("left", "right", "up", "down")
 	#velocity = input_direction * SPEED
@@ -49,13 +49,13 @@ func movement():
 #calling functions!
 func _physics_process(_delta: float) -> void:
 	if facing_down:
-		$AnimatedSprite2D.frame = 2
-	elif facing_up:
-		$AnimatedSprite2D.frame = 1
-	elif facing_right:
 		$AnimatedSprite2D.frame = 3
-	elif facing_left:
+	if facing_up:
 		$AnimatedSprite2D.frame = 0
+	if facing_right:
+		$AnimatedSprite2D.frame = 1
+	if facing_left:
+		$AnimatedSprite2D.frame = 2
 	movement()
 	move_and_slide()
 	check_health()
